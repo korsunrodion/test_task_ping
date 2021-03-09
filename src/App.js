3d2cfd6bac5296e67d4c39656fc6dfdd641f7e84
Rodion Korsun <korsun.rodion@gmail.com>
2021-03-09 04:11:46 +0200

    last

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Ping from './ping'
 import './App.css';
 
+// корректно работает для сайтов, у которых доступен favicon под стандартным именем /favicon.ico
 function App() {
+  const [url, setUrl] = useState('')
+  const [status, setStatus] = useState(' ')
+
+  function handleChange(event) {
+    setUrl(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let updUrl;
+    if (!(url.startsWith('http://') || url.startsWith('https://'))) {
+      updUrl = 'http://' + url;
+    } else {
+      updUrl = url;
+    }
+
+    if(/https?:\/\/.+\..+/.test(updUrl) === false) {
+      setStatus('Wrong url')
+      return;
+    }
+
+    setStatus('Checking..')
+
+    var p = new Ping({multiplier: 0.3});
+    p.ping(updUrl, (err, data) => {
+      if (err) {
+        setStatus("Were unable to ping server");
+      } else {
+        setStatus(`Took ${data} ms`);
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Ping cheker</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <label>Address:</label>
+        <input type="text" value={url} onChange={handleChange} />
+        <input type="submit" value="Check" />
+      </form>
+      <p className="status">{status}</p>
     </div>
   );
 }
